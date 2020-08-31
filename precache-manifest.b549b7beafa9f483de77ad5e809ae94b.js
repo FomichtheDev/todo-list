3d542fcd8c9719f4d216cd741f239bd4ae68fd04
FomichtheDev <fomichg@gmail.com>
2020-08-31 21:39:11 +0300

    Updates

---
+++ precache-manifest.b549b7beafa9f483de77ad5e809ae94b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e972d5183fcc8ffbbcdf331d9940185",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "bd7cadb15e4b75444b20",
+    "url": "/todo-list/static/css/main.4917a1fa.chunk.css"
+  },
+  {
+    "revision": "e75d17c9d54390f62477",
+    "url": "/todo-list/static/js/2.1af08d20.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/todo-list/static/js/2.1af08d20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd7cadb15e4b75444b20",
+    "url": "/todo-list/static/js/main.70fea812.chunk.js"
+  },
+  {
+    "revision": "4c207b602d40809001a5",
+    "url": "/todo-list/static/js/runtime-main.2929926a.js"
+  }
+]);
